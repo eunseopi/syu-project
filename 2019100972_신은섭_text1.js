12953031746fd5a3ae87b714a945b5fe6cef57c1
EunSeop Shin <asxzqw12@naver.com>
2023-03-26 12:23:55 +0900

    first commit

---
+++ 2019100972_신은섭_text1.js
@@ -0,0 +1,12 @@
+for(let i = 0; i < 5; i++)
+{
+    for(let j = i; j < 5- 1; j++)
+    {
+        console.log(" ");
+    }
+    for(let k = 1; k <= i + 1; k++)
+    {
+        console.log(k);
+    }
+    console.log("\n");
+}
